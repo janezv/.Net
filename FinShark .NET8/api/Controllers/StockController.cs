6e99b74dc805bfc163b5f7bab635dd25c529ca54
Janez Vegan <janez.vegan@gmail.com>
2024-12-17 21:50:05 +0100

    put and post methods

--- FinShark .NET8/api/Controllers/StockController.cs
+++ FinShark .NET8/api/Controllers/StockController.cs
@@ -1,7 +1,10 @@
 ﻿using api.Data;
 using Microsoft.AspNetCore.Mvc;
-using api.Dtos;
 using api.Mapper;
+using api.Dtos.Stock;
+using System;                  // Pogosto za osnovne tipe in izjeme
+using System.Collections.Generic; // Če uporabljate kolekcije, kot so seznami ali slovarji
+
 
 namespace api.Controllers
 {
@@ -31,6 +34,36 @@ namespace api.Controllers
                 return NotFound();
             }
             return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            _context.stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id=stockModel.Id},stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public  IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto) 
+        {
+            var stockModel=_context.stocks.FirstOrDefault(x=>x.Id==id);
+            if (stockModel == null) 
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+            //stockModel.stockUp = stock.ToStockFromUpdateStockRequestDto();
+            //_context.stocks.Update(stockModel);
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
         }
     }
 }
--- FinShark .NET8/api/Mapper/StockMappers.cs
+++ FinShark .NET8/api/Mapper/StockMappers.cs
@@ -1,6 +1,7 @@
 ﻿using api.Models;
 using api.Dtos;
 using api.Dtos.Stock;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Mapper
 {
@@ -17,6 +18,32 @@ namespace api.Mapper
                 LastDiv= sotckModel.LastDiv,
                 Industry= sotckModel.Industry,
                 MarketCap= sotckModel.MarketCap,
+            };
+        }
+
+        public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto) 
+        {
+            return new Stock
+            {
+                 Symbol = stockDto.Symbol,
+                 CompanyName= stockDto.CompanyName,
+                 Purchase=stockDto.Purchase,
+                 LastDiv=stockDto.LastDiv,
+                 Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromUpdateStockRequestDto(this UpdateStockRequestDto stockDto) 
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
             };
         }
     }
