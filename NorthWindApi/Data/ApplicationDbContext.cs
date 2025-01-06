c9668ad44a2167ac887f5b09e9fd3ab474fc0d36
Janez Vegan <janez.vegan@gmail.com>
2025-01-06 21:49:23 +0100

    Dodan projekt NorthWind

---
+++ NorthWindApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorthWindApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+      //  public DbSet<YourEntity> YourEntities { get; set; }  // Zamenjajte z vašimi entitetami
+    }
+}
