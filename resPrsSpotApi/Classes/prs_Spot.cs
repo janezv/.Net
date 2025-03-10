59290e0ba5588e1e9261631890256c50c0748ea6
Janez Vegan <janez.vegan@ajpes.si>
2025-03-10 16:08:36 +0100

    Dodana Post metoda ki vrne list prs_spot

--- resPrsSpotApi/Classes/prs_Spot.cs
+++ resPrsSpotApi/Classes/prs_Spot.cs
@@ -2,7 +2,7 @@
 {
     public class prs_Spot
     {
-        public double maticnaEnotaField { get; set; }
+        public int maticnaEnotaField { get; set; }
         public string popolnoImeField { get; set; }
         public string kratkoImeField { get; set; }
     }
--- resPrsSpotApi/Controllers/HomeController.cs
+++ resPrsSpotApi/Controllers/HomeController.cs
@@ -1,41 +1,71 @@
 using Microsoft.AspNetCore.Mvc;
 using resPrsSpotApi.Models;
 using resPrsSpotApi.Classes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System;
 
-namespace resPrsSpotApi.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class HomeController : ControllerBase
+namespace resPrsSpotApi.Controllers
 {
-    private static readonly string[] Summaries = new[]
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-    private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
 
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
+        [HttpGet("PozdravljenSvet")]
+        public IActionResult Hw()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpPost("get")]
+        public IActionResult Get([FromBody] GetMyModel x, [FromQuery] int y, [FromQuery] int z)
+        {
+            var result = new prs_Spot
+            {
+                maticnaEnotaField = 12,
+                popolnoImeField = "ahoj",
+                kratkoImeField = "adfsd"
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPost("getList")]
+        public IActionResult GetList([FromBody] GetMyModel x, [FromQuery] string y)
+        {
+            List<prs_Spot> rez = new List<prs_Spot>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int randomValue = GetRandomInteger(0, 10);
+                rez.Add(new prs_Spot
+                {
+                    maticnaEnotaField = randomValue,
+                    popolnoImeField = "ahoj",
+                    kratkoImeField = "adfsd"
+                });
+            }
+
+            return Ok(rez);
+        }
+
+        static int GetRandomInteger(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1); // max + 1 da vkljuèimo zgornjo mejo
+        }
     }
-
-    [HttpGet("PozdravljenSvet")]
-    public async Task<string> hw()
-    {
-        return "Hello World";
-    }
-
-    [HttpPost(Name = "get")]
-    public async Task<Classes.prs_Spot> Get([FromBody] GetMyModel x, [FromQuery] int y, [FromQuery] int z)
-    {
-       
-        //string iErr = null;
-        //GetClientCert(ref iErr);
-        return new Classes.prs_Spot { maticnaEnotaField=12.2, popolnoImeField="ahoj", kratkoImeField="adfsd" };
-    }
-
-
-
- }
+}
