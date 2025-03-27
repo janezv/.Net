e8b1c99f82b4562c8f2974a67a226952b5fe682f
Janez Vegan <janez.vegan@gmail.com>
2025-03-27 21:16:47 +0100

    Nastavitve react-a, sprememba porta in kreiranja header

---
+++ .Net 8 React PS/globo/ReactWeb/src/main/App.tsx
@@ -0,0 +1,14 @@
+// import "bootstrap/dist/scc/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="Providing houses all over the world"></Header>
+    </div>
+  );
+}
+
+export default App;
